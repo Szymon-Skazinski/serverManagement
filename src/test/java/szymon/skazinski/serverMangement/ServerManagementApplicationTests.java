15e07caffd04a5b82c24858de12191caee38f5b2
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2022-01-27 20:00:58 +0100

    Initial commit

---
+++ src/test/java/szymon/skazinski/serverMangement/ServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package szymon.skazinski.serverMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
